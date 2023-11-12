7ff75cc129c5df8c2202b6ff83bfab0ce7f69c71
SaharMFR <sweetysahora@gmail.com>
2023-11-12 15:14:20 -0800

    update in task-5

--- models/__init__.py
+++ models/__init__.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 """For the package"""
-from models.engine.file_storage.py import FileStorage
+from models.engine.file_storage import FileStorage
 storage = FileStorage()
 storage.reload()
